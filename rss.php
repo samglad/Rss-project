d4319dd3ff8285796dd4cc49355e3e57ee3b7fb8
Samuel Komolafe <sammyglad1@gmail.com>
2024-01-04 18:16:02 +0100

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,6 +1,6 @@
 <?php
-// Server-Side: PHP Code to Fetch the RSS Feed
-$rss_url = '$rss_url = 'https://anchor.fm/s/eff9f688/podcast/rss';
+// fetch_data.php
+$rss_url = 'https://anchor.fm/s/eff9f688/podcast/rss';
 $rss = simplexml_load_file($rss_url);
-$feed = json_encode($rss);
+echo json_encode($rss);
 ?>
